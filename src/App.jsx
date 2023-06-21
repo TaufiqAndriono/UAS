07197928292db8f5fcf2103bc6a58f1be047d4d9
bakaanano <taufiqandriono688@gmail.com>
2023-06-21 23:04:12 +0700

    tes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Component, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import section1 from './komponen/section1'
+
+class App extends Components {
+  render() {
+  return (
+    <>
+    <div>
+      <section1 />
+    </div>
+      
+    </>
+  );
+}
+}
+
+export default App
---
+++ src/komponen/section1.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+
+class section1 extends Component{
+render()  {
+  return 
+      <div>
+        <h1>
+        Testing
+        </h1>
+      </div>
+    
+  }
+}
+
+
+
+export default section1
